45657dbc1d44c79702e117b76953fc24462b96dd
dawidniziol <dawid.niziol@gmail.com>
2023-01-24 17:44:32 +0100

    First commit

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from app.models.projects import Stage
 from app.models.projects import Order
+from app.models.projects import Offers
 
 
 @admin.register(Stage)
@@ -12,5 +13,18 @@ class StageAdmin(admin.ModelAdmin):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
 
-    list_display = ['name', 'stage', 'created_at', 'deadline', 'completed']
-    list_filter = ['stage']
+    list_display = ['name',
+                    'stage',
+                    'deadline_invitations',
+                    'completed_invitations',
+                    'angels_extras',
+                    'deadline_extras',
+                    'completed_extras']
+
+    list_filter = ['completed_invitations',
+                   'completed_extras',
+                   'stage', ]
+
+@admin.register(Offers)
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ['name', 'samples', ]
